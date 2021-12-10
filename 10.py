4b6f58ac200fd7c4fdd2e9e7ed3c601e349d7d26
lw-jcardenas <jcardenas@liquidweb.com>
2021-12-10 17:31:55 -0600

    10 part 2

--- 10.py
+++ 10.py
@@ -16,16 +16,20 @@
 with open('10_in.txt') as f:
   lines = f.read()
 
+# lines = "<{([{{}}[<[[[<>{}]]]>[]]"
+
+
 def parse_line(line):
   if not line:
-    return True
+    return 0
   loc = 0
   done = False
   stack = []
   openers = "([{<"
   closers = ")]}>"
-  points = [3, 57, 1197, 25137]
-  print(line)
+  err_point_board = [3, 57, 1197, 25137]
+  inc_point_board = [ 1, 2, 3, 4 ]
+  # print(line)
   while not done:
     # print(loc, line[loc])
     if line[loc] in openers:
@@ -35,7 +39,7 @@ def parse_line(line):
       if closers[brace_id] != line[loc]:
         err = "Expected {}, but found {} instead.".format(closers[brace_id], line[loc])
         e = SyntaxError(err)
-        e.points = points[ closers.find(line[loc]) ]
+        e.points = err_point_board[ closers.find(line[loc]) ]
         raise e
       stack = stack[:-1]
     else:
@@ -43,18 +47,28 @@ def parse_line(line):
     loc += 1
     if loc >= len(line):
       done = True
-  return True
+  inc_points = 0
+  for c in stack[::-1]:
+    inc_points = inc_points * 5 + inc_point_board[openers.find(c)]
+  return inc_points
 
-score = 0
+err_score = 0
+inc_scores = []
 for line in lines.split('\n'):
+  if not line:
+    continue
   try:
     # print(parse_line(line))
-    parse_line(line)
+    inc_points = parse_line(line)
+    print(inc_points)
+    inc_scores.append(inc_points)
   except SyntaxError as e:
     print(e)
     print(e.points)
-    score += e.points
+    err_score += e.points
 
-print(score)
-
+print("Syntax score: {}".format(err_score))
+inc_scores.sort()
+mid = int((len(inc_scores) - 1) / 2)
+print("Completion score: {}".format(inc_scores[mid]))
 
